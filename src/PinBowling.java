95bfa2dfba3c5fbf40bd5c0939c0624fc59ceec6
Komarala <komaralahemanth@gmail.com>
2023-09-14 00:56:55 +0200

    code updated to run with runtime inputs from one player

--- src/PinBowling.java
+++ src/PinBowling.java
@@ -2,81 +2,112 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 //Class for calculating Bowling Game score
 public class PinBowling{
-    private List<Integer> rolls = new ArrayList<>();
+    //List<Integer> rolls = new ArrayList<>();
 
-    public void roll(int pins) {
-        rolls.add(pins);
-    }
-
-    public int calculateScore() {
+    //public void roll(int pins) {
+    //    rolls.add(pins);
+    //}
+    public int calculateScore(List<Integer>TotalRolls) {
         int score = 0;
         int rollIndex = 0;
+        System.out.println(TotalRolls);
 
-        for (int frame = 0; frame < 10; frame++) {
-            if (isStrike(rollIndex)) {
-                score += 10 + strikeBonus(rollIndex);
+        for (int frame = 1; frame <= 10; frame++) {
+            if (isStrike(rollIndex, TotalRolls)) {
+                score += 10 + strikeBonus(rollIndex, TotalRolls);
                 rollIndex++;
-            } else if (isSpare(rollIndex)) {
-                score += 10 + spareBonus(rollIndex);
+            } else if (isSpare(rollIndex,TotalRolls)) {
+                score += 10 + spareBonus(rollIndex, TotalRolls);
                 rollIndex += 2;
             } else {
-                score += frameScore(rollIndex);
+                score += frameScore(rollIndex, TotalRolls);
                 rollIndex += 2;
             }
-        }
-
+            }
+            System.out.println("FrameScore "+score);
         return score;
     }
     
     //Returns Scored points of each roll in list
-    public List<Integer> Board(){
-        return rolls;
+    public List<Integer> Board(List<Integer>TotalRolls){
+        return TotalRolls;
     }
 
     //Check if Strike
-    private boolean isStrike(int rollIndex) {
-        return rolls.get(rollIndex) == 10;
+    public boolean isStrike(int rollIndex, List<Integer>TotalRolls) {
+        return TotalRolls.get(rollIndex) == 10;
     }
 
     //Check if Spare
-    private boolean isSpare(int rollIndex) {
-        return rolls.get(rollIndex) + rolls.get(rollIndex + 1) == 10;
+    public boolean isSpare(int rollIndex,List<Integer>TotalRolls) {
+        return TotalRolls.get(rollIndex) + TotalRolls.get(rollIndex+1) == 10;
     }
 
     //Add Strike bonus
-    private int strikeBonus(int rollIndex) {
-        return rolls.get(rollIndex + 1) + rolls.get(rollIndex + 2); 
+    private int strikeBonus(int rollIndex,List<Integer>TotalRolls) {
+        return TotalRolls.get(rollIndex + 1) + TotalRolls.get(rollIndex + 2); 
     }
 
     //Add Spare bonus
-    private int spareBonus(int rollIndex) {
-        return rolls.get(rollIndex + 2);
+    private int spareBonus(int rollIndex,List<Integer>TotalRolls) {
+        return TotalRolls.get(rollIndex + 2);
     }
 
     //Total score calculator
-    private int frameScore(int rollIndex) {
-        return rolls.get(rollIndex) + rolls.get(rollIndex + 1);
+    private int frameScore(int rollIndex,List<Integer>TotalRolls) {
+        return TotalRolls.get(rollIndex) + TotalRolls.get(rollIndex + 1);
     }
 
-    //Main Source Code
+        //Main Source Code
     public static void main(String[] args) {
         PinBowling game = new PinBowling();
+        int rollsAllowed = 2;
+        List<Integer> rolls = new ArrayList<>();
 
-        // Example Player's rolls
-        int[] rolls = {1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6};
+        try (Scanner sc = new Scanner(System.in)) {
+            for (int frame = 1; frame <= 10; frame++) {
+                System.out.println("Frame " + (frame));
 
-        //Looping for rolls
-        for (int pins : rolls) {
-            game.roll(pins);
+                for (int i = 1; i < rollsAllowed; i++){
+                    System.out.print("Enter pins knocked down of Frame " + frame + " in roll " + (i)+" : ");
+                    int FirstRoll = sc.nextInt();
+                    rolls.add(FirstRoll);
+                    if (FirstRoll==10){
+                        System.out.println("Strike!");
+                    }
+                    else {
+                        System.out.print("Enter pins knocked down of Frame " + frame + " in roll " + (i+1)+" : ");
+                        int SecondRoll = sc.nextInt();
+                        rolls.add(SecondRoll);
+                        if(FirstRoll + SecondRoll == 10){  
+                        System.out.println("Spare!");
+                        if (frame==10){
+                            System.out.print("Enter pins knocked down of Frame " + frame + " in roll " + (i+2)+" : ");
+                            int ThirdRoll = sc.nextInt();
+                            rolls.add(ThirdRoll);
+                        }
+                        }
+                    }
+                    if (frame==10 && FirstRoll==10){
+                        System.out.print("Enter pins knocked down of Frame " + frame + " in roll " + (i+1)+" : ");
+                        int SecondRoll = sc.nextInt();
+                        rolls.add(SecondRoll);
+                        System.out.print("Enter pins knocked down of Frame " + frame + " in roll " + (i+2)+" : ");
+                        int ThirdRoll = sc.nextInt();
+                        rolls.add(ThirdRoll);
+                        }
+                }
+            }
         }
-
-        List <Integer> Board = game.Board();
-        int score = game.calculateScore();
+        
+        System.out.println(rolls);
+        int score = game.calculateScore(rolls);
+        List <Integer> Board = game.Board(rolls);
         System.out.println("Score Board = " + Board);
         System.out.println("Player's score: " + score);
     }
 }
-
